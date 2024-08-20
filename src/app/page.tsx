55b39f1fd3196bedfc3d7ee00a179f51da5dd65c
kodaliakhil <kodaliakhil1999@gmail.com>
2024-08-20 21:49:44 +0530

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const { userId } = auth();
+  const isAuth = !!userId;
   return (
-    <Button>Click me</Button>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton /> {/* afterSignOutUrl is deprecated */}
+          </div>
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, reserchers, and professionals to
+            instantly answer questions and understand reserceh with AI.
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>file upload</h1>
+            ) : (
+              <Link href={"/sign-in"}>
+                <Button>Login to get Started!</Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
