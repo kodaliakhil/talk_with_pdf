9f67b1836932c55a065b018e9895a1e756a2dfc0
kodaliakhil <kodaliakhil1999@gmail.com>
2024-08-23 16:37:52 +0530

    messages schema, drizzle config setup incomplete

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,14 @@
-import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgEnum,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const userSystemEnum = pgEnum("user_system_enum", ["system", "user"]);
 
 export const chats = pgTable("chats", {
   id: serial("id").primaryKey(), // Every Chat will have a unique id
@@ -8,3 +18,16 @@ export const chats = pgTable("chats", {
   userId: varchar("user_id", { length: 256 }).notNull(), // The id of the user who created the chat
   fileKey: text("file_key").notNull(), // The key of the pdf file we uploaded. It will be used to locate the pdf file in S3
 });
+
+export const messages = pgTable("messages", {
+  id: serial("id").primaryKey(),
+  chatId: integer("chat_id")
+    .references(() => chats.id)
+    .notNull(), // The id of the chat the message belongs to. "references" is used to create a foreign key
+  content: text("content").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  role: userSystemEnum("role").notNull(),
+});
+
+
+// drizzle kit will allow us to migrate all the schema to the database
