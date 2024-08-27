2b087d802bc76f5eae4b7d2bbee81c6c30dca937
kodaliakhil <kodaliakhil1999@gmail.com>
2024-08-27 20:04:05 +0530

    drizzle setup and migrate models

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 - Clerk: For Authentication
 - Neon DB: Serverless Postgres for Database
 - DrizzleORM: Query builder
+
+
+
+`npx drizzle-kit push` for migrating the models to DB
+`npx drizzle-kit studio` to view the data
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,22 @@
-import type { Config } from "drizzle-kit";
+// import type { Config } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit'
 import * as dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
-export default {
-  driver: "pg",
+// export default {
+//   driver: "pg",
+//   schema: "./src/lib/db/schema.ts",
+//   dbCredentials: {
+//     url: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
+
+export default defineConfig({
   schema: "./src/lib/db/schema.ts",
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+  verbose: true,
+  strict: true,
+})
