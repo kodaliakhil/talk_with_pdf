74958c662b37a0e68a9f6669d04da1a85b06d43e
kodaliakhil <kodaliakhil1999@gmail.com>
2024-09-12 20:26:43 +0530

    chat functionality with open ai gpt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,8 +1,10 @@
 import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
 export const runtime = "edge";
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPEN_AI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
@@ -10,7 +12,14 @@ const openai = new OpenAIApi(config);
 export async function POST(req: Request) {
   try {
     const { messages } = await req.json();
-    const response = await openai.createChatCompletion
-    // .......................................
-  } catch (error) {}
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages,
+      stream: true,
+    });
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.log("Error in /api/chat------>", error);
+  }
 }
